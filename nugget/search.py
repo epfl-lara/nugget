11f8d3995eaceb76e0bff8790e88eb8e4c6436be
Romain Edelmann <romain.edelmann@gmail.com>
2018-03-29 16:56:06 +0200

    Add a first version of batched best first search

--- nugget/search.py
+++ nugget/search.py
@@ -77,6 +77,87 @@ def best_first_search(from_expr, to_expr, heuristics, factor=0.0):
     return (path, actions, history)
 
 
+def batch_best_first_search(from_expr, to_expr, heuristics,
+                            factor=0.0, batch_size=32):
+
+    if from_expr == to_expr:
+        return ([from_expr], [], [])
+
+    h = heuristics.with_target_batch(to_expr)
+    threshold = batch_size - (len(transformations) / 2)
+
+    parents = { from_expr: (None, None) }
+    to_estimate = [(from_expr, 0, None, None)]
+    to_visit = []
+
+    # For logging purposes.
+    ids = {}
+    history = []
+    next_id = 0
+
+    while to_estimate or to_visit:
+
+        if len(to_estimate) > threshold:
+            exprs = [x[0] for x in to_estimate]
+            ds = h(exprs)
+            for ((expr, depth, t, parent_id), d) in zip(to_estimate, ds):
+                heappush(to_visit, (d + depth * factor, (expr, depth, t, parent_id)))
+                ids[expr] = next_id
+                history.append((next_id, expr, d, t, parent_id))
+                next_id += 1
+            to_estimate = []
+
+        if not to_estimate:
+            max_priority = None
+            for i in range(max(round(threshold / len(transformations)), 1)):
+                (priority, entry) = heappop(to_visit)
+                to_estimate.append(entry)
+
+                if max_priority is None:
+                    max_priority = priority
+
+                if priority >= max_priority + 1:
+                    break
+
+        (expr, depth, transformation, parent_id) = to_estimate.pop(0)
+
+        if expr not in ids:
+            ids[expr] = next_id
+            history.append((next_id, expr, None, transformation, parent_id))
+            next_id += 1
+
+        child_depth = depth + 1
+        for t, f in transformations_functions.items():
+            child_expr = f(expr)
+            if child_expr is not None and not child_expr in parents:
+                to_estimate.append((child_expr, child_depth, t, ids[expr]))
+                parents[child_expr] = (expr, t)
+                if child_expr == to_expr:
+                    for (other_expr,
+                         other_depth,
+                         other_transformation,
+                         other_parent_id) in to_estimate:
+
+                        if other_expr not in ids:
+                            ids[other_expr] = next_id
+                            history.append((
+                                next_id,
+                                other_expr,
+                                None,
+                                other_transformation,
+                                other_parent_id))
+                            next_id += 1
+
+                    path = [to_expr]
+                    actions = []
+                    current_expr = to_expr
+                    while current_expr is not None:
+                        (current_expr, action) = parents[current_expr]
+                        if current_expr is not None:
+                            path.insert(0, current_expr)
+                            actions.insert(0, action)
+                    return (path, actions, history)
+
 def breadth_first_search(from_expr, to_expr):
     if from_expr == to_expr:
         return ([from_expr], [], [])
